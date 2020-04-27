d93ff93b33664582c493c5f7812d4c1af60da018
Audun Wigum Arbo <audunwar@stud.ntnu.no>
2020-04-27 19:35:51 +0200

    Revert package upgrades

---
+++ VinmonopoletArchiver/Entities/BooleanOrFalseConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.TypeConversion;
+
+namespace VinmonopoletArchiver.Entities
+{
+    internal class BooleanOrFalseConverter : ITypeConverter
+    {
+        public string ConvertToString(object value, IWriterRow row, MemberMapData memberMapData)
+        {
+            return value.ToString();
+        }
+
+        public object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
+        {
+            if (bool.TryParse(text, out var value))
+                return value;
+            return false;
+        }
+
+        public bool CanConvertFrom(Type type)
+        {
+            return type == typeof(string);
+        }
+
+        public bool CanConvertTo(Type type)
+        {
+            return type == typeof(bool);
+        }
+    }
+}
